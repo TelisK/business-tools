7882f82241fe3aedc418ad2852892d0f264b8031
TelisK <tkoutsogiannakis@hotmail.com>
2026-04-05 15:28:00 +0300

    postgres

--- business_tools/settings.py
+++ business_tools/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'business_tools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_data' / 'db.sqlite3',
+        'ENGINE': os.environ.get('POSTGRES_ENGINE','django.db.backends.sqlite3'),
+        'NAME': os.environ.get('POSTGRES_DB',BASE_DIR / 'db_data' / 'db.sqlite3'),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
