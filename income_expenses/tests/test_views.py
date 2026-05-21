e9db908e48d6718d92913b636f881860bbc9286a
TelisK <tkoutsogiannakis@hotmail.com>
2026-05-21 07:38:10 +0300

    testing continues

--- income_expenses/tests/test_views.py
+++ income_expenses/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
+from income_expenses.models import Store, Income
 
 class AuthenticationTest(TestCase):
     def setUp(self):
@@ -15,9 +16,59 @@ class AuthenticationTest(TestCase):
         response = self.client.post(reverse('accounts:register'), {
             'username' : 'newuser',
             'email' : 'test@email.com',
-            'password1' : 'testpass',
-            'password2' : 'testpass'
+            'password1' : 'VeryStrongPassword123!@#',
+            'password2' : 'VeryStrongPassword123!@#'
         })
         
         new_user = User.objects.filter(username='newuser')
         self.assertTrue(new_user.exists())
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+        def test_login_with_correct_credentials(self):
+            logged_in = self.client.login(username='testuser', password='testpass123')
+            self.assertTrue(logged_in)
+
+        def test_login_with_wrong_password(self):
+            logged_in = self.client.login(username='testuser1', password='testpass1234')
+            self.assertFalse(logged_in)
+
+        def test_redirect_after_successful_login(self):
+            response = self.client.post(reverse('accounts:login'), {
+            'username' : 'testuser',
+            'password' : 'testpass123'
+        })
+            self.assertEqual(response.status_code, 302)
+
+class SecurityTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+        self.store = Store.objects.create(
+            name='test store',
+            user=self.user
+        )
+    
+    def test_if_different_user_has_access(self):
+        user2 = User.objects.create_user(
+            username='user2',
+            password='user2pass'
+        )
+
+        client2 = Client()  # Need seperate client for diferrent user
+        client2.login(username='user2', password='user2pass')
+
+        store_url = reverse('income_expenses:detail', args=[self.store.id]) # args puts the id on the url's id section
+        response = client2.get(store_url)
+
+        self.assertNotEqual(response.status_code, 200)
