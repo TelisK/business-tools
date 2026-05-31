5b79c3cf1d2ccce69d8335642dfd26ee728521af
TelisK <tkoutsogiannakis@hotmail.com>
2026-05-31 22:17:32 +0300

    celery corrections after failure on testing

--- business_tools/settings.py
+++ business_tools/settings.py
@@ -181,7 +181,8 @@ if 'test' in sys.argv:
 
     
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
 from celery.schedules import crontab
 
