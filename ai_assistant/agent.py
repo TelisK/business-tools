790d4027ae0bd12ce96f71c992628f60fe101022
TelisK <tkoutsogiannakis@hotmail.com>
2026-07-07 23:39:49 +0300

    ai assistant - model works perfectly through shell. Need to make the chat page

--- ai_assistant/agent.py
+++ ai_assistant/agent.py
@@ -1,6 +1,7 @@
 from google import genai
 from google.genai import types
-from income_expenses.views import get_totals, last_years_income_comparison, income_totals_calculation
+from income_expenses.views import last_years_income_comparison
+from .tools import ai_assist_totals
 import os
 from dotenv import load_dotenv
 
@@ -17,7 +18,7 @@ api_key_str = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key_str)
 
 def gemini_agent(store, question):
-    tools = [get_totals, last_years_income_comparison]
+    tools = [ai_assist_totals, last_years_income_comparison]
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
@@ -36,52 +37,9 @@ def gemini_agent(store, question):
 
             print(f"[DEBUG] Το Gemini αποφάσισε να καλέσει το εργαλείο: {name} με ορίσματα {args}")
             if name == 'get_totals':
-                result = get_totals(store=store.id)
+                result = ai_assist_totals(store=store.id)
             elif name == 'last_years_income_comparison':
                 result = last_years_income_comparison(store=store.id)
-            elif name == 'income_totals_calculation':
-                result = income_totals_calculation(store=store.id)
-
-        # We have to create a chat history, so at the next response, ai will
-        # be able to see the previous conversation.
-        chat_history = [
-                types.Content(role="user", parts=[types.Part.from_text(text=question)]),
-                response.candidates[0].content,  # Gemini calls function call
-                types.Content(
-                    role="tool", 
-                    parts=[
-                        types.Part.from_function_response(
-                            name=name,
-                            response={"result": result}  # result of Django ORM
-                        )
-                    ]
-                )
-        ]
-
-        second_response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents=chat_history,
-        config=types.GenerateContentConfig(
-            system_instruction=INSTRUCTION,
-            tools=tools,
-        ),
-        )
-        
-        return second_response.text
-
+            # elif name == 'income_totals_calculation':
+            #     result = income_totals_calculation(store=store.id)
     return response.text
-
-'''
-AΠΟΤΕΛΕΣΜΑΤΑ ΜΕΣΩ ΤΟΥ SHELL.
->>> result_1 = gemini_agent(store=store_obj, question="Ποια είναι τα συνολικά έσοδα του καταστήματος απο 1 ιανουαρίου 2026 μέχρι σήμερα;")
-INFO 2026-07-07 01:02:59,677 models: AFC is enabled with max remote calls: 10.
-INFO 2026-07-07 01:03:02,374 _client: HTTP Request: POST https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent "HTTP/1.1 200 OK"
->>> print(f"Απάντηση Agent: {result_1}")
-Απάντηση Agent: Παρακαλώ δώστε μου το αναγνωριστικό του καταστήματος (store ID) για να σας εμφανίσω τα συνολικά έσοδα από 1 Ιανουαρίου 2026 έως και σήμερα.
->>> result_1 = gemini_agent(store=store_obj, question=f"Ποια είναι τα συνολικά έσοδα του καταστήματος με id {store_obj.i
-d} απο 1 ιανουαρίου 2026 μέχρι σήμερα;")
-INFO 2026-07-07 01:03:59,737 models: AFC is enabled with max remote calls: 10.
-INFO 2026-07-07 01:04:08,869 _client: HTTP Request: POST https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent "HTTP/1.1 200 OK"
-INFO 2026-07-07 01:04:08,893 models: AFC remote call 1 is done.
-Traceback (most recent call last):
-'''
--- ai_assistant/tools.py
+++ ai_assistant/tools.py
@@ -1,2 +1,44 @@
-# For start we have already some function on the income_expenses view,
-# which we will use.
+from income_expenses.views import get_totals
+'''
+Fetches the economic summary. Income, Expenses, FPA Tax, Income analysis with cash, pos, etc.
+YTD result and YTD total's analysis.
+for a specific store, a specific timeline. 
+
+Args:
+    store: The unique ID (integer) of the store.
+    date_from: The start date of the timeline in 'DD-MM-YYYY' format.
+    date_to: The end date of the timeline in 'DD-MM-YYYY' format.
+'''
+
+# Bridge function with get_totals. Gemini don't recognise Dataframes, and I don't need them.
+def ai_assist_totals(store: int, date_from: str, date_to: str):
+    sum_income_result, sum_expenses_result, expenses_fpa, income_totals, YTD_result, \
+    YTD_totals, income_df, expenses_df, income_result, expenses_result = get_totals(
+        store=store,
+        date_from=date_from,
+        date_to=date_to
+)
+
+    return {
+            'sum_income': float(sum_income_result),
+            'sum_expenses': float(sum_expenses_result),
+            'expenses_fpa': float(expenses_fpa),
+            'income_totals': {
+                'cash': float(income_totals.get('total_cash')),  # maybe needs a ',0' in case it's None?
+                'pos': float(income_totals.get('total_pos')),
+                'deposit': float(income_totals.get('total_deposit')),
+                'check': float(income_totals.get('total_check')),
+                'other': float(income_totals.get('total_other'))
+            },
+            'YTD_result': float(YTD_result),
+            'YTD_totals': {
+                'cash': float(YTD_totals.get('total_cash')),
+                'pos': float(YTD_totals.get('total_pos')),
+                'deposit': float(YTD_totals.get('total_deposit')),
+                'check': float(YTD_totals.get('total_check')),
+                'other': float(YTD_totals.get('total_other'))
+            }
+            # 'income_result': float(income_result),  # QUERYSET
+            # 'expenses_result': float(expenses_result)
+    }
+    
