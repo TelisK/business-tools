27d601dafc547f82e75d3992b0e1672a3fe50289
TelisK <tkoutsogiannakis@hotmail.com>
2026-07-12 00:00:03 +0300

    start the correction on db, filter supplier with the afm number.

--- invoices/models.py
+++ invoices/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from income_expenses.models import Store, Expenses
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Supplier(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    # db_index links with the other table and is faster.
+    afm = models.CharField(max_length=9, unique=True, db_index=True)
+    supplier = models.CharField(max_length=200)
+
 class Invoice(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    # PROTECT because Supplier will be common with all users who have the same. We don't want to be deleted.
+    new_supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, null=True, blank=True)
+    supplier = models.CharField(max_length=200)
     expense = models.OneToOneField(Expenses, on_delete=models.CASCADE, blank=True, null=True)
     invoice_number = models.CharField(null=False, max_length=50)
-    afm = models.CharField(max_length=9)
-    supplier = models.CharField(max_length=200)
     date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     fpa = models.DecimalField(max_digits=10, decimal_places=2)
+    fpa_persentage = models.IntegerField(default=24, 
+                            validators=[MinValueValidator(0), MaxValueValidator(100)])
     total = models.DecimalField(max_digits=10, decimal_places=2)
     file = models.FileField(upload_to='invoices/', null=True, blank=True)
     paid = models.BooleanField(default=False)
